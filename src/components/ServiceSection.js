9d52f5346abe595ea8fb57191fe3509d3225772a
Pandu Ananto <pandunih@gmail.com>
2022-07-13 00:13:30 +0700

    services tab content on desktop view

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
 import { Tab } from '@headlessui/react';
+import { HiStar } from 'react-icons/hi';
 
 import { services } from '../data';
 import classNames from '../utils/classNames';
 
 function ServiceSection() {
 	return (
-		<div className="mt-6 w-full md:mt-10">
+		<div className="mt-6 grid w-full grid-cols-12 gap-4 md:mt-10">
 			<Tab.Group>
-				<Tab.List className="flex space-x-1 overflow-y-auto rounded bg-white p-1">
+				<Tab.List className="col-span-3 space-y-2 self-start">
 					{Object.keys(services).map((service) => (
 						<Tab
 							key={service}
 							className={({ selected }) =>
 								classNames(
 									classNames(
-										'w-full whitespace-nowrap rounded py-2.5 px-3 text-sm font-semibold leading-5 text-white',
-										'ring-white ring-offset-2 ring-offset-red-600 focus:outline-none focus:ring-2',
-										selected ? 'bg-red-100 text-red-600' : 'text-slate-400 hover:text-red-500'
+										'w-full whitespace-nowrap rounded py-4 px-5 text-sm font-semibold leading-5 text-white',
+										'focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-slate-50',
+										selected ? 'bg-red-600 text-white' : 'text-slate-500 hover:bg-slate-200 hover:text-slate-600'
 									)
 								)
 							}
@@ -27,6 +28,31 @@ function ServiceSection() {
 						</Tab>
 					))}
 				</Tab.List>
+				<Tab.Panels className="col-span-9">
+					{Object.values(services).map((posts, index) => (
+						<Tab.Panel key={`posts-${index}`}>
+							{posts.map((post) => (
+								<div key={`post-${post.id}`} className="space-y-4">
+									<img
+										src={post.images}
+										className="h-96 w-full rounded bg-no-repeat object-cover object-center"
+										alt={`${post.id}`}
+									/>
+									<h3 className="text-2xl font-bold text-slate-900">{post.title}</h3>
+									<p className="text-justify text-lg font-light text-slate-700">{post.description}</p>
+									<ul className="space-y-2">
+										{post.benefits.map((benefit) => (
+											<li className="flex flex-row items-center space-x-2">
+												<HiStar className="h-6 w-6 text-orange-500"></HiStar>
+												<p className="font-medium">{benefit.description}</p>
+											</li>
+										))}
+									</ul>
+								</div>
+							))}
+						</Tab.Panel>
+					))}
+				</Tab.Panels>
 			</Tab.Group>
 		</div>
 	);
