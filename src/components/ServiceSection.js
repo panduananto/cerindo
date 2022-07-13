44c62ffd398e97941fbca4916de9059383d19938
Pandu Ananto <pandunih@gmail.com>
2022-07-14 00:01:16 +0700

    service content responsive layout

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -10,16 +10,18 @@ function ServiceSection() {
 	return (
 		<div className="mt-6 grid w-full grid-cols-12 gap-4 md:mt-10">
 			<Tab.Group>
-				<Tab.List className="col-span-3 space-y-2 self-start">
+				<Tab.List className="col-span-12 flex space-y-2 self-start overflow-x-auto xl:col-span-3 xl:block xl:overflow-x-visible">
 					{Object.keys(services).map((service) => (
 						<Tab
 							key={service}
 							className={({ selected }) =>
 								classNames(
 									classNames(
-										'w-full whitespace-nowrap rounded py-4 px-5 text-sm font-semibold leading-5 text-white',
-										'focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-slate-50',
-										selected ? 'bg-red-600 text-white' : 'text-slate-500 hover:bg-slate-200 hover:text-slate-600'
+										'w-full whitespace-nowrap rounded-r py-4 px-5 text-left text-sm font-semibold leading-5 text-white',
+										'focus:outline-none focus:ring-4 focus:ring-red-300',
+										selected
+											? 'border-l-4 border-red-600 bg-red-100 text-red-600'
+											: 'text-slate-500 hover:bg-slate-200 hover:text-slate-600'
 									)
 								)
 							}
@@ -28,26 +30,28 @@ function ServiceSection() {
 						</Tab>
 					))}
 				</Tab.List>
-				<Tab.Panels className="col-span-9">
+				<Tab.Panels className="col-span-12 xl:col-span-9">
 					{Object.values(services).map((posts, index) => (
 						<Tab.Panel key={`posts-${index}`}>
 							{posts.map((post) => (
-								<div key={`post-${post.id}`} className="space-y-4">
+								<div key={`post-${post.id}`} className="grid grid-cols-8 overflow-hidden rounded shadow">
 									<img
 										src={post.images}
-										className="h-96 w-full rounded bg-no-repeat object-cover object-center"
+										className="col-span-8 h-64 w-full bg-no-repeat object-cover object-center md:col-span-4 md:h-full"
 										alt={`${post.id}`}
 									/>
-									<h3 className="text-2xl font-bold text-slate-900">{post.title}</h3>
-									<p className="text-justify text-lg font-light text-slate-700">{post.description}</p>
-									<ul className="space-y-2">
-										{post.benefits.map((benefit) => (
-											<li className="flex flex-row items-center space-x-2">
-												<HiStar className="h-6 w-6 text-orange-500"></HiStar>
-												<p className="font-medium">{benefit.description}</p>
-											</li>
-										))}
-									</ul>
+									<div className="col-span-8 rounded-br rounded-bl border-l border-r border-b border-slate-300 bg-white py-4 px-4 text-slate-900 md:col-span-4 md:rounded-bl-none md:rounded-tr md:border-l-0 md:border-t md:px-6 md:py-8">
+										<h3 className="font-rubik text-lg font-bold md:text-xl">{post.title}</h3>
+										<p className="mt-2 text-justify text-sm md:mt-4 md:text-base">{post.description}</p>
+										<ul className="mt-4 space-y-2 md:mt-6">
+											{post.benefits.map((benefit) => (
+												<li key={benefit.description} className="flex flex-row items-start space-x-3">
+													<HiStar className="h-5 w-5 flex-shrink-0 text-red-600 md:h-7 md:w-7"></HiStar>
+													<p className="text-sm text-slate-600 md:text-base">{benefit.description}</p>
+												</li>
+											))}
+										</ul>
+									</div>
 								</div>
 							))}
 						</Tab.Panel>
