d988a83f9ae0dc5c23970c43b3bace95a6101d48
Pandu Ananto <panduanantoh@gmail.com>
2024-02-07 20:50:52 +0700

    extract user account navigation to UserAccountNav component

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -9,19 +9,8 @@ import { siteConfig } from '@/config/site'
 
 import useBoundedScroll from '@/hooks/use-bounded-scroll'
 
-import SignoutButton from '../auth/signout-button'
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
 import { Button } from '../ui/button'
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuGroup,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from '../ui/dropdown-menu'
-import Icons from '../ui/icons'
+import UserAccountNav from '../user-account-nav'
 import MainNav from './main-nav'
 import MobileNav from './mobile-nav'
 
@@ -49,45 +38,7 @@ const SiteHeader = ({ user }: SiteHeaderProps) => {
 				</div>
 				<div className="ml-auto">
 					{user ? (
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<Button variant="secondary" className="relative size-8 rounded-full">
-									<Avatar>
-										{user?.user_metadata.avatar_url ? (
-											<AvatarImage src={user.user_metadata.avatar_url} alt={`${user.email} profile picture`} />
-										) : (
-											<AvatarFallback>
-												<Icons.user className="size-5" />
-												<span className="sr-only">{user.email}</span>
-											</AvatarFallback>
-										)}
-									</Avatar>
-								</Button>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent className="w-56" align="end" forceMount>
-								<DropdownMenuLabel className="font-normal">
-									<div className="flex flex-col space-y-1">
-										<p>Signed in as</p>
-										<p className="mt-1.5 text-sm font-medium leading-none">{user.email}</p>
-									</div>
-								</DropdownMenuLabel>
-								<DropdownMenuSeparator />
-								<DropdownMenuGroup>
-									<DropdownMenuItem asChild>
-										<Link href="/dashboard">
-											<Icons.dashboard className="mr-2 size-4" aria-hidden="true" />
-											Dashboard
-										</Link>
-									</DropdownMenuItem>
-								</DropdownMenuGroup>
-								<DropdownMenuSeparator />
-								<DropdownMenuGroup>
-									<DropdownMenuItem asChild>
-										<SignoutButton className="w-full" />
-									</DropdownMenuItem>
-								</DropdownMenuGroup>
-							</DropdownMenuContent>
-						</DropdownMenu>
+						<UserAccountNav user={user} />
 					) : (
 						<Button>
 							<Link href="/signin">
